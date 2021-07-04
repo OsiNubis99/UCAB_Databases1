fb40797a255276980820e3503a2b6735a8f6bab2
Alexis F. Quiros A <aquiros@MSC.DOM>
2021-07-04 09:20:32 -0400

    los modelos pero aun sin campos

--- AA/models.py
+++ AA/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class Artista(models.Model):
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    nombreart=models.CharField(max_length=50)
+
+    
+class Pintura(models.Model):
+    pass
+
+class Moneda(models.Model):
+    pass
+
+class Tienda(models.Model):
+    pass
+
+class Contacto_Organizacion(models.Model):
+    pass
+
+class Coleccionista(models.Model):
+    pass
+
+class Cliente(models.Model):
+    pass
+
+class Pais(models.Model):
+    pass
+
+class Participante(models.Model):
+    pass
+
+class Factura(models.Model):
+    pass
+
+class Reglonfactura(models.Model):
+    pass
+
+class Subasta_Tienda (models.Model):
+    pass
+
+class Subasta_Evento(models.Model):
+    pass
+
+class Articulo_Subasta(models.Model):
+    pass
