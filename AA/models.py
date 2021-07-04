955c441d9b24ae780019fa59d555bdf39aff2dd2
Alexis F. Quiros A <aquiros@MSC.DOM>
2021-07-04 12:34:13 -0400

    agregado los atributos de los modelos pero sin las relaciones

--- AA/models.py
+++ AA/models.py
@@ -8,7 +8,7 @@ class Artista(models.Model):
     apellido=models.CharField(max_length=50)
     nombreart=models.CharField(max_length=50)
 
-    
+
 class Pintura(models.Model):
     pass
 
@@ -16,28 +16,63 @@ class Moneda(models.Model):
     pass
 
 class Tienda(models.Model):
-    pass
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    pagina=models.CharField(max_length=50)
+    proposito=models.CharField(max_length=100)
+    alcance=models.CharField(max_length=50)
+    tipos=[
+        ('Antiguedades','antiguedades'),
+        ('Galeria','galeria'),
+        ('Numismatica','numismatica')
+
+    ]
+    tipo=models.CharField(max_length=70,choices=tipos)
+    lugar = models.ForeignKey(Pais)
 
 class Contacto_Organizacion(models.Model):
-    pass
+    id=models.AutoField(primary_key=True)
+    lugar = models.ForeignKey(Tienda)
+    cargo=models.CharField(max_length=50)
+    nombre=models.CharField(max_length=20)
+    apellido=models.CharField(max_length=20)
+    email=models.CharField(max_length=50)
+    telefonoid=models.CharField(max_length=50)
+
 
 class Coleccionista(models.Model):
-    pass
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    telefonoid=models.CharField(max_length=50)
+    email=models.CharField(max_length=13)
+    lugar = models.ForeignKey(Pais)
 
 class Cliente(models.Model):
-    pass
+    date_client = models.DateField(null=True, blank=True)
+
 
 class Pais(models.Model):
-    pass
+    id=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    nacionalidad=models.CharField(max_length=50)
+
 
 class Participante(models.Model):
-    pass
+    id=models.AutoField(primary_key=True)
+    coleccionista_id=models.ForeignKey(Coleccionista)
+    subasta_id=models.ForeignKey(Subasta_Evento)
+    factura_id=models.ForeignKey(Factura)
+    factura_inscripcion_id=models.ForeignKey(Factura)
 
 class Factura(models.Model):
-    pass
+    id=models.AutoField(primary_key=True)
+    iva=models.IntegerField()
 
 class Reglonfactura(models.Model):
-    pass
+    factura_id = models.ForeignKey(Factura,on_delete=models.CASCADE)
+    articulo_id = models.ForeignKey(Articulo_Subasta,on_delete=models.CASCADE)
+    precio=models.IntegerField()
+    descripcion=models.CharField(max_length=50)
 
 class Subasta_Tienda (models.Model):
     pass
@@ -46,4 +81,8 @@ class Subasta_Evento(models.Model):
     pass
 
 class Articulo_Subasta(models.Model):
-    pass
+    ask=models.IntegerField()
+    bid=models.IntegerField()
+    por_ganancia=models.IntegerField()
+    precio_alcanzado=models.IntegerField()
+    duracion_min=models.IntegerField()
