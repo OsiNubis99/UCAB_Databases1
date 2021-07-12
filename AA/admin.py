dacd75903a709939ae326ea99fe742572c384889
Andrés David Hurtado Fernández <OsiNubis99@PM.me>
2021-07-12 10:38:28 -0400

    feat: All model added to admin

--- AA/admin.py
+++ AA/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Articulo_Subasta
+from .models import Artista
+from .models import Catalogo_Moneda
+from .models import Catalogo_Pintura
+from .models import Cliente
+from .models import Coleccionista
+from .models import Contacto_tienda
+from .models import Costo_envio
+from .models import Divisa
+from .models import Factura
+from .models import Moneda
+from .models import Pais
+from .models import Participante
+from .models import Reglonfactura
+from .models import Subasta_Evento
+from .models import Tienda
 
 # Register your models here.
+
+admin.site.register(Articulo_Subasta)
+admin.site.register(Artista)
+admin.site.register(Catalogo_Moneda)
+admin.site.register(Catalogo_Pintura)
+admin.site.register(Cliente)
+admin.site.register(Coleccionista)
+admin.site.register(Contacto_tienda)
+admin.site.register(Costo_envio)
+admin.site.register(Divisa)
+admin.site.register(Factura)
+admin.site.register(Moneda)
+admin.site.register(Pais)
+admin.site.register(Participante)
+admin.site.register(Reglonfactura)
+admin.site.register(Subasta_Evento)
+admin.site.register(Tienda)
