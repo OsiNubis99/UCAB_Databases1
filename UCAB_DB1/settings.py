4facbb3c920b735e7a72221786ec032958c7be58
Andrés David Hurtado Fernández <OsiNubis99@PM.me>
2021-07-15 18:52:16 -0400

    fix: Updated settings.py

--- UCAB_DB1/settings.py
+++ UCAB_DB1/settings.py
@@ -32,8 +32,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_interface',
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AA',
-    'colorfield', 
-    
     # 'geeks'
 ]
 X_FRAME_OPTIONS='SAMEORIGIN' # only if django version >= 3.0
