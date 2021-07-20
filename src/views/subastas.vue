acfff8d21f636c4b6b37e53462279017b50237a8
Andrés David Hurtado Fernández <OsiNubis99@PM.me>
2021-07-20 10:15:15 -0400

    filtro por fecha

--- src/views/subastas.vue
+++ src/views/subastas.vue
@@ -2,11 +2,18 @@
 	<v-container fluid ma-3>
 		<v-layout row wrap align-center justify-center>
 			<v-flex xs11>
+				<v-text-field
+					v-model="dateRangeText"
+					label="Date range"
+					prepend-icon="mdi-calendar"
+					readonly
+				></v-text-field>
 				<v-data-table
 					:headers="headers"
-					:items="subastas"
+					:items="filtersubastas"
+					sort-by="fecha"
 					:items-per-page="15"
-					class="elevation-1"
+					class="elevation-1 mb-5"
 				>
 					<template v-slot:item.actions="{ item }">
 						<v-btn color="black" text :to="'/Subasta/' + item.id">
@@ -17,6 +24,16 @@
 						{{ item.fecha.slice(0, 10) }}
 					</template>
 				</v-data-table>
+				<v-date-picker
+					v-model="dates"
+					range
+					full-width
+					:min="
+						new Date(Date.now() + new Date().getTimezoneOffset() * 60000 * 5)
+							.toISOString()
+							.substr(0, 10)
+					"
+				></v-date-picker>
 			</v-flex>
 		</v-layout>
 	</v-container>
@@ -43,12 +60,32 @@ export default {
 			{ text: "Actions", value: "actions", sortable: false },
 		],
 		subastas: [],
+		dates: [],
 	}),
+	computed: {
+		dateRangeText() {
+			return this.dates.join(" ~ ");
+		},
+		filtersubastas() {
+			if (this.dates.length == 2)
+				return this.subastas.filter((subasta) => {
+					var l1 = new Date(this.dates[0]);
+					var l2 = new Date(this.dates[1]);
+					if (l1 > l2) {
+						var l3 = l1;
+						l1 = l2;
+						l2 = l3;
+					}
+					var t = new Date(subasta.fecha.slice(0, 10));
+					return l1 <= t && t <= l2;
+				});
+			return this.subastas;
+		},
+	},
 	created() {
 		this.axios
 			.get("http://localhost:4000/api/subasta/getAll")
 			.then((response) => {
-				console.log(response.data);
 				this.subastas = response.data;
 			})
 			.catch((error) => {
